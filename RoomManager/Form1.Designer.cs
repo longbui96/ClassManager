7f9ef2bd1b8eaf3769552867d14c5966fa499ed3
pm0906 <35551485+pm0906@users.noreply.github.com>
2018-03-19 18:31:13 +0700

    form giao dien

---
+++ RoomManager/Form1.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace RoomManager
+{
+    partial class btnQL
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.btnM = new System.Windows.Forms.Button();
+            this.btnThoat = new System.Windows.Forms.Button();
+            this.btnT = new System.Windows.Forms.Button();
+            this.btnPH = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Tahoma", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.button1.Location = new System.Drawing.Point(70, 80);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(181, 58);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Quản lý";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnM
+            // 
+            this.btnM.Font = new System.Drawing.Font("Tahoma", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.btnM.Location = new System.Drawing.Point(70, 144);
+            this.btnM.Name = "btnM";
+            this.btnM.Size = new System.Drawing.Size(86, 58);
+            this.btnM.TabIndex = 2;
+            this.btnM.Text = "Mượn";
+            this.btnM.UseVisualStyleBackColor = true;
+            this.btnM.Click += new System.EventHandler(this.btnM_Click);
+            // 
+            // btnThoat
+            // 
+            this.btnThoat.Font = new System.Drawing.Font("Tahoma", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.btnThoat.Location = new System.Drawing.Point(70, 272);
+            this.btnThoat.Name = "btnThoat";
+            this.btnThoat.Size = new System.Drawing.Size(181, 58);
+            this.btnThoat.TabIndex = 3;
+            this.btnThoat.Text = "Thoát";
+            this.btnThoat.UseVisualStyleBackColor = true;
+            this.btnThoat.Click += new System.EventHandler(this.btnThoat_Click);
+            // 
+            // btnT
+            // 
+            this.btnT.Font = new System.Drawing.Font("Tahoma", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.btnT.Location = new System.Drawing.Point(162, 144);
+            this.btnT.Name = "btnT";
+            this.btnT.Size = new System.Drawing.Size(89, 58);
+            this.btnT.TabIndex = 4;
+            this.btnT.Text = "Trả";
+            this.btnT.UseVisualStyleBackColor = true;
+            this.btnT.Click += new System.EventHandler(this.btnT_Click);
+            // 
+            // btnPH
+            // 
+            this.btnPH.Font = new System.Drawing.Font("Tahoma", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.btnPH.Location = new System.Drawing.Point(70, 208);
+            this.btnPH.Name = "btnPH";
+            this.btnPH.Size = new System.Drawing.Size(181, 58);
+            this.btnPH.TabIndex = 5;
+            this.btnPH.Text = "Tình Trạng Phòng";
+            this.btnPH.UseVisualStyleBackColor = true;
+            this.btnPH.Click += new System.EventHandler(this.btnPH_Click);
+            // 
+            // label1
+            // 
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.label1.Font = new System.Drawing.Font("Tahoma", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Image = global::RoomManager.Properties.Resources._04;
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(313, 68);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Quản lý phòng học";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnQL
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.ClientSize = new System.Drawing.Size(313, 342);
+            this.Controls.Add(this.btnPH);
+            this.Controls.Add(this.btnT);
+            this.Controls.Add(this.btnThoat);
+            this.Controls.Add(this.btnM);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Name = "btnQL";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Quản lý phòng học";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.btnQL_FormClosing);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnM;
+        private System.Windows.Forms.Button btnThoat;
+        private System.Windows.Forms.Button btnT;
+        private System.Windows.Forms.Button btnPH;
+    }
+}
+
---
+++ RoomManager/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace RoomManager
+{
+    public partial class btnQL : Form
+    {
+        public btnQL()
+        {
+            InitializeComponent();
+        }
+
+        private void btnM_Click(object sender, EventArgs e)
+        {
+            Muon m= new Muon();
+            m.ShowDialog();
+        }
+
+        private void btnT_Click(object sender, EventArgs e)
+        {
+            DSTraPhong t = new DSTraPhong();
+            t.ShowDialog();
+        }
+
+        private void btnThoat_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            QuanLy q = new QuanLy();
+            q.ShowDialog();
+        }
+
+        private void btnPH_Click(object sender, EventArgs e)
+        {
+            PhongHu p = new PhongHu();
+            p.ShowDialog();
+        }
+
+        private void btnQL_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (MessageBox.Show("Bạn muốn đóng chương trình ?", "Thoát", MessageBoxButtons.YesNo) == DialogResult.No)
+                e.Cancel = true;
+        }
+     
+    }
+}
