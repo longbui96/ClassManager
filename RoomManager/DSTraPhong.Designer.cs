5d408b5fefcc61fb026b3e722429086c42da20b4
pm0906 <35551485+pm0906@users.noreply.github.com>
2018-03-20 09:52:50 +0700

    Danh sach tra phong

---
+++ RoomManager/DSTraPhong.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace RoomManager
+{
+    partial class DSTraPhong
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvTKB = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dtpPT = new System.Windows.Forms.DateTimePicker();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cbbPhong = new System.Windows.Forms.ComboBox();
+            this.btnTim = new System.Windows.Forms.Button();
+            this.btnTra = new System.Windows.Forms.Button();
+            this.btnQL = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvTKB)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvTKB
+            // 
+            this.dgvTKB.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvTKB.Dock = System.Windows.Forms.DockStyle.Top;
+            this.dgvTKB.Location = new System.Drawing.Point(0, 0);
+            this.dgvTKB.Name = "dgvTKB";
+            this.dgvTKB.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvTKB.Size = new System.Drawing.Size(1044, 266);
+            this.dgvTKB.TabIndex = 1;
+            this.dgvTKB.SelectionChanged += new System.EventHandler(this.dgvTKB_SelectionChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.label1.Location = new System.Drawing.Point(13, 308);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(42, 16);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "Trong";
+            // 
+            // dtpPT
+            // 
+            this.dtpPT.CustomFormat = "dd/MM/yyyy";
+            this.dtpPT.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dtpPT.Location = new System.Drawing.Point(69, 304);
+            this.dtpPT.Name = "dtpPT";
+            this.dtpPT.Size = new System.Drawing.Size(121, 20);
+            this.dtpPT.TabIndex = 11;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.label2.Location = new System.Drawing.Point(12, 285);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(43, 16);
+            this.label2.TabIndex = 14;
+            this.label2.Text = "Phòng";
+            // 
+            // cbbPhong
+            // 
+            this.cbbPhong.FormattingEnabled = true;
+            this.cbbPhong.Location = new System.Drawing.Point(69, 280);
+            this.cbbPhong.Name = "cbbPhong";
+            this.cbbPhong.Size = new System.Drawing.Size(121, 21);
+            this.cbbPhong.TabIndex = 13;
+            // 
+            // btnTim
+            // 
+            this.btnTim.Font = new System.Drawing.Font("Tahoma", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.btnTim.Location = new System.Drawing.Point(196, 281);
+            this.btnTim.Name = "btnTim";
+            this.btnTim.Size = new System.Drawing.Size(76, 43);
+            this.btnTim.TabIndex = 15;
+            this.btnTim.Text = "Tìm";
+            this.btnTim.UseVisualStyleBackColor = true;
+            this.btnTim.Click += new System.EventHandler(this.btnTim_Click);
+            // 
+            // btnTra
+            // 
+            this.btnTra.Font = new System.Drawing.Font("Tahoma", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.btnTra.Location = new System.Drawing.Point(644, 281);
+            this.btnTra.Name = "btnTra";
+            this.btnTra.Size = new System.Drawing.Size(76, 43);
+            this.btnTra.TabIndex = 16;
+            this.btnTra.Text = "Trả";
+            this.btnTra.UseVisualStyleBackColor = true;
+            this.btnTra.Click += new System.EventHandler(this.btnTra_Click);
+            // 
+            // btnQL
+            // 
+            this.btnQL.Font = new System.Drawing.Font("Tahoma", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.btnQL.Location = new System.Drawing.Point(938, 281);
+            this.btnQL.Name = "btnQL";
+            this.btnQL.Size = new System.Drawing.Size(95, 43);
+            this.btnQL.TabIndex = 17;
+            this.btnQL.Text = "Quay lại";
+            this.btnQL.UseVisualStyleBackColor = true;
+            this.btnQL.Click += new System.EventHandler(this.btnQL_Click);
+            // 
+            // DSTraPhong
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1044, 340);
+            this.Controls.Add(this.btnQL);
+            this.Controls.Add(this.btnTra);
+            this.Controls.Add(this.btnTim);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.cbbPhong);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dtpPT);
+            this.Controls.Add(this.dgvTKB);
+            this.Name = "DSTraPhong";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "DSTraPhong";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvTKB)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvTKB;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DateTimePicker dtpPT;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox cbbPhong;
+        private System.Windows.Forms.Button btnTim;
+        private System.Windows.Forms.Button btnTra;
+        private System.Windows.Forms.Button btnQL;
+    }
+}
---
+++ RoomManager/DSTraPhong.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace RoomManager
+{
+    public partial class DSTraPhong : Form
+    {
+        public class TKB 
+        {
+            //
+            public int IDPhong { get; set; }
+            public string Phong { get; set; }
+            public string Lop { get; set; }
+            public string Mon { get; set; }
+            public int TietBD { get; set; }
+            public int TietKT { get; set; }
+            public DateTime NgayMuon { get; set; }
+            public string NguoiMuon { get; set; }
+            public string NguoiTra { get; set; }
+            public string LyDo { get; set; }
+
+            //
+            public TKB(int Id, string Phong, string Lop, string Mon, int TietBD, int TietKT, DateTime NgayMuon, string NguoiMuon, string NguoiTra, string LyDo)
+            {
+                this.IDPhong = Id;
+                this.Phong = Phong;
+                this.Lop = Lop;
+                this.Mon = Mon;
+                this.TietBD = TietBD;
+                this.TietKT = TietKT;
+                this.NgayMuon = NgayMuon;
+                this.NguoiMuon = NguoiMuon;
+                this.NguoiTra = NguoiTra;
+                this.LyDo = LyDo;
+            }
+        }
+        public class cbbItem
+        {
+            //
+            public string Text { get; set; }
+            public object Value { get; set; }
+
+            public cbbItem(string t, object v)
+            {
+                this.Text = t;
+                this.Value = v;
+            }
+        }
+
+        SqlConnection cn = new SqlConnection();
+        public DSTraPhong()
+        {
+            InitializeComponent();
+            try
+            {
+                string cnstr = "Server = .; Database = QLPH; Integrated Security = true;";
+                cn.ConnectionString = cnstr;
+                if (cn != null && cn.State == ConnectionState.Closed)
+                {
+                    cn.Open();
+                }
+            }
+            catch (SqlException)
+            {
+                MessageBox.Show("Loi ket noi !!", "Thong bao", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            finally
+            {
+                cn.Close();
+            }
+
+            //////////////////////////////////
+
+            List<cbbItem> list = new List<cbbItem>();
+            cn.Open();
+            string name;
+            object v;
+            string sql = "select MaPhong, TenPhong from Phong";
+            SqlCommand cmd = new SqlCommand(sql, cn);
+            SqlDataReader dr = cmd.ExecuteReader();
+            cbbItem t = new cbbItem("Tất cả","0");
+            list.Add(t);
+            while (dr.Read())
+            {
+                v = Convert.ToString(dr.GetInt32(0));
+                name = dr.GetString(1);
+                cbbItem item = new cbbItem(name, v);
+                list.Add(item);
+            }
+            cbbPhong.DataSource = list;
+            cbbPhong.DisplayMember = "Text";
+            cbbPhong.ValueMember = "Value";
+            dr.Close();
+            cn.Close();
+        }
+        public void Getdata()
+        {
+            cn.Open();
+            string sql;
+            cbbItem cbb = (cbbItem)cbbPhong.SelectedItem;
+            string value = cbb.Value.ToString();
+            if (Convert.ToInt32(value) == 0)
+                sql = "select TKB.MaPhong, P.TenPhong, LH.TenLop, M.TenMon, TKB.TietBD, TKB.TietKT, TKB.Ngay, MP.TenNgMuon, TP.TenNgTra , MP.Note from TKBieu TKB, MuonPhong MP, TraPhong TP, Phong P, LopHoc LH,Mon M where  TKB.MaMP like MP.MaMP and TKB.MaTP like TP.MaTP and TKB.MaPhong like P.MaPhong and TKB.MaLH like LH.MaLH and LH.MaMon like M.MaMon";
+            else
+                sql = "select TKB.MaPhong, P.TenPhong, LH.TenLop, M.TenMon, TKB.TietBD, TKB.TietKT, TKB.Ngay, MP.TenNgMuon, TP.TenNgTra , MP.Note from TKBieu TKB, MuonPhong MP, TraPhong TP, Phong P, LopHoc LH,Mon M where  TKB.MaMP like MP.MaMP and TKB.MaTP like TP.MaTP and TKB.MaPhong like P.MaPhong and TKB.MaLH like LH.MaLH and LH.MaMon like M.MaMon and TKB.MaPhong like " + value;
+            List<TKB> list = new List<TKB>();
+            SqlCommand cmd = new SqlCommand(sql, cn);
+            SqlDataReader dr = cmd.ExecuteReader();
+            int TietBD, TietKT, IDPhong;
+            DateTime NgayMuon;
+            string NguoiMuon, NguoiTra, LyDo, Mon, Phong, Lop;
+            while (dr.Read())
+            {
+                IDPhong = dr.GetInt32(0); 
+                Phong = dr.GetString(1);
+                Lop = dr.GetString(2);
+                Mon = dr.GetString(3);
+                TietBD = dr.GetInt32(4);
+                TietKT = dr.GetInt32(5);
+                NgayMuon = dr.GetDateTime(6);
+                NguoiMuon = dr.GetString(7);
+                NguoiTra = dr.GetString(8);
+                LyDo = dr.GetString(9);
+                if (NgayMuon.Date == Convert.ToDateTime(dtpPT.Text))
+                {
+                    TKB tkbieu = new TKB(IDPhong, Phong, Lop, Mon, TietBD, TietKT, NgayMuon, NguoiMuon, NguoiTra, LyDo);
+                    list.Add(tkbieu);
+                }
+            }
+            dr.Close();
+            dgvTKB.DataSource = list;
+            cn.Close();
+        }
+        private void btnTim_Click(object sender, EventArgs e)
+        {
+            Getdata();
+        }
+        int rNum = -1;
+        private void dgvTKB_SelectionChanged(object sender, EventArgs e)
+        {
+            rNum = dgvTKB.CurrentRow.Index;
+        }
+        public static int GIDPhong, GTietBD, GTietKT;
+        public static DateTime GNgay;
+
+        private void btnTra_Click(object sender, EventArgs e)
+        {
+            GIDPhong = Convert.ToInt32(dgvTKB.Rows[rNum].Cells[0].Value.ToString());
+            GTietBD = Convert.ToInt32(dgvTKB.Rows[rNum].Cells[4].Value.ToString());
+            GTietKT = Convert.ToInt32(dgvTKB.Rows[rNum].Cells[5].Value.ToString());
+            GNgay = Convert.ToDateTime(dgvTKB.Rows[rNum].Cells[6].Value);
+            Tra Tra = new Tra(this);
+            Tra.ShowDialog();
+        }
+
+        private void btnQL_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }  
+    }
+}
