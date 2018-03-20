51e97d444e4a514bbc2b1e7511ad21b541d7988e
pm0906 <35551485+pm0906@users.noreply.github.com>
2018-03-20 10:01:20 +0700

    Form muon phong

---
+++ RoomManager/Muon.Designer.cs
@@ -0,0 +1,358 @@
+﻿namespace RoomManager
+{
+    partial class Muon
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cbbPhong = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.panelSV = new System.Windows.Forms.Panel();
+            this.txtMa = new System.Windows.Forms.TextBox();
+            this.lblH = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.radSV = new System.Windows.Forms.RadioButton();
+            this.radGV = new System.Windows.Forms.RadioButton();
+            this.txtTen = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.cbbLaplai = new System.Windows.Forms.ComboBox();
+            this.cbbTietKT = new System.Windows.Forms.ComboBox();
+            this.cbbTietBD = new System.Windows.Forms.ComboBox();
+            this.txtLyDo = new System.Windows.Forms.TextBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.panelSV.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // cbbPhong
+            // 
+            this.cbbPhong.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbbPhong.FormattingEnabled = true;
+            this.cbbPhong.Location = new System.Drawing.Point(65, 137);
+            this.cbbPhong.Name = "cbbPhong";
+            this.cbbPhong.Size = new System.Drawing.Size(134, 24);
+            this.cbbPhong.TabIndex = 6;
+            this.cbbPhong.Text = "P001";
+            this.cbbPhong.MouseEnter += new System.EventHandler(this.cbbPhong_MouseEnter);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 136);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 16);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Phòng";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.panelSV);
+            this.groupBox1.Controls.Add(this.panel1);
+            this.groupBox1.Controls.Add(this.txtTen);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(12, 164);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(289, 133);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Người mượn";
+            // 
+            // panelSV
+            // 
+            this.panelSV.Controls.Add(this.txtMa);
+            this.panelSV.Controls.Add(this.lblH);
+            this.panelSV.Location = new System.Drawing.Point(6, 89);
+            this.panelSV.Name = "panelSV";
+            this.panelSV.Size = new System.Drawing.Size(277, 35);
+            this.panelSV.TabIndex = 6;
+            // 
+            // txtMa
+            // 
+            this.txtMa.Location = new System.Drawing.Point(81, 3);
+            this.txtMa.Name = "txtMa";
+            this.txtMa.Size = new System.Drawing.Size(183, 23);
+            this.txtMa.TabIndex = 0;
+            // 
+            // lblH
+            // 
+            this.lblH.AutoSize = true;
+            this.lblH.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblH.Location = new System.Drawing.Point(7, 8);
+            this.lblH.Name = "lblH";
+            this.lblH.Size = new System.Drawing.Size(41, 16);
+            this.lblH.TabIndex = 7;
+            this.lblH.Text = "MaGV";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.radSV);
+            this.panel1.Controls.Add(this.radGV);
+            this.panel1.Location = new System.Drawing.Point(58, 52);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(212, 31);
+            this.panel1.TabIndex = 1;
+            // 
+            // radSV
+            // 
+            this.radSV.AutoSize = true;
+            this.radSV.Location = new System.Drawing.Point(128, 3);
+            this.radSV.Name = "radSV";
+            this.radSV.Size = new System.Drawing.Size(78, 20);
+            this.radSV.TabIndex = 1;
+            this.radSV.Text = "Sinh viên";
+            this.radSV.UseVisualStyleBackColor = true;
+            this.radSV.CheckedChanged += new System.EventHandler(this.radSV_CheckedChanged);
+            // 
+            // radGV
+            // 
+            this.radGV.AutoSize = true;
+            this.radGV.Checked = true;
+            this.radGV.Location = new System.Drawing.Point(3, 3);
+            this.radGV.Name = "radGV";
+            this.radGV.Size = new System.Drawing.Size(85, 20);
+            this.radGV.TabIndex = 1;
+            this.radGV.TabStop = true;
+            this.radGV.Text = "Giảng viên";
+            this.radGV.UseVisualStyleBackColor = true;
+            // 
+            // txtTen
+            // 
+            this.txtTen.Location = new System.Drawing.Point(58, 20);
+            this.txtTen.Name = "txtTen";
+            this.txtTen.Size = new System.Drawing.Size(212, 23);
+            this.txtTen.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(13, 25);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(30, 16);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Tên";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label7);
+            this.groupBox2.Controls.Add(this.dateTimePicker1);
+            this.groupBox2.Controls.Add(this.label6);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.cbbLaplai);
+            this.groupBox2.Controls.Add(this.cbbTietKT);
+            this.groupBox2.Controls.Add(this.cbbTietBD);
+            this.groupBox2.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox2.Location = new System.Drawing.Point(12, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(289, 121);
+            this.groupBox2.TabIndex = 6;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Thời gian";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(12, 58);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(73, 16);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "Ngày mượn";
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(96, 58);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(174, 23);
+            this.dateTimePicker1.TabIndex = 11;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(12, 88);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(45, 16);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Lập lại";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(101, 88);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(33, 16);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "tuần";
+            // 
+            // cbbLaplai
+            // 
+            this.cbbLaplai.FormattingEnabled = true;
+            this.cbbLaplai.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6"});
+            this.cbbLaplai.Location = new System.Drawing.Point(65, 85);
+            this.cbbLaplai.Name = "cbbLaplai";
+            this.cbbLaplai.Size = new System.Drawing.Size(30, 24);
+            this.cbbLaplai.TabIndex = 9;
+            this.cbbLaplai.Text = "1";
+            // 
+            // cbbTietKT
+            // 
+            this.cbbTietKT.FormattingEnabled = true;
+            this.cbbTietKT.Location = new System.Drawing.Point(155, 25);
+            this.cbbTietKT.Name = "cbbTietKT";
+            this.cbbTietKT.Size = new System.Drawing.Size(115, 24);
+            this.cbbTietKT.TabIndex = 8;
+            this.cbbTietKT.Text = "Tiết kết thúc";
+            this.cbbTietKT.MouseEnter += new System.EventHandler(this.cbbTietKT_MouseEnter);
+            // 
+            // cbbTietBD
+            // 
+            this.cbbTietBD.FormattingEnabled = true;
+            this.cbbTietBD.Location = new System.Drawing.Point(15, 25);
+            this.cbbTietBD.Name = "cbbTietBD";
+            this.cbbTietBD.Size = new System.Drawing.Size(114, 24);
+            this.cbbTietBD.TabIndex = 7;
+            this.cbbTietBD.Text = "Tiết bắt đầu";
+            // 
+            // txtLyDo
+            // 
+            this.txtLyDo.Location = new System.Drawing.Point(15, 25);
+            this.txtLyDo.Multiline = true;
+            this.txtLyDo.Name = "txtLyDo";
+            this.txtLyDo.Size = new System.Drawing.Size(255, 67);
+            this.txtLyDo.TabIndex = 0;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.txtLyDo);
+            this.groupBox3.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox3.Location = new System.Drawing.Point(12, 303);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(289, 98);
+            this.groupBox3.TabIndex = 1;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Lý do";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(44, 407);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(88, 30);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "OK";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(183, 407);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(88, 30);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Quay lại";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Muon
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(313, 442);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cbbPhong);
+            this.Name = "Muon";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Mượn phòng";
+            this.Load += new System.EventHandler(this.Muon_Load_1);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.panelSV.ResumeLayout(false);
+            this.panelSV.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cbbPhong;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.RadioButton radSV;
+        private System.Windows.Forms.RadioButton radGV;
+        private System.Windows.Forms.TextBox txtTen;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.ComboBox cbbTietKT;
+        private System.Windows.Forms.ComboBox cbbTietBD;
+        private System.Windows.Forms.TextBox txtLyDo;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Panel panelSV;
+        private System.Windows.Forms.TextBox txtMa;
+        private System.Windows.Forms.Label lblH;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ComboBox cbbLaplai;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Label label7;
+    }
+}
---
+++ RoomManager/Muon.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace RoomManager
+{
+    public partial class Muon : Form
+    {
+        SqlConnection cn;
+        SqlCommand cm;
+        SqlDataAdapter ap;
+        DataSet ds;
+        public Muon()
+        {
+            InitializeComponent();
+        }
+        private void radSV_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radSV.Checked == true)
+                lblH.Text = "MSSV";
+           
+            else if (radSV.Checked == false)
+                lblH.Text = "MaGV";
+        }
+        public void connect()
+        {
+            string cntr = "Server = .; Database = QLPH; Integrated Security = true;";
+            cn = new SqlConnection(cntr);
+            try
+            {
+                if (cn != null && cn.State == ConnectionState.Closed)
+                    cn.Open();
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+        public void disconnect()
+        {
+            if (cn != null && cn.State == ConnectionState.Open)
+            {
+                cn.Close();
+            }
+        }
+        private void Muon_Load_1(object sender, EventArgs e)
+        {
+            TimeSt();
+            Timekt();
+        }
+        public void TimeSt()
+        {
+            connect();
+            cm = new SqlCommand("select MaTiet, TenTiet From Tiet Order by MaTiet ASC", cn);
+            ap = new SqlDataAdapter(cm);
+            ds = new DataSet();
+            ap.Fill(ds, "Tiet");
+            cbbTietBD.DataSource = ds.Tables[0];
+            cbbTietBD.DisplayMember = "TenTiet";
+            cbbTietBD.ValueMember = "MaTiet";
+            disconnect();
+        }
+        public void Timekt()
+        {
+            connect();
+            cm = new SqlCommand("select MaTiet, TenTiet From Tiet where MaTiet >" + cbbTietBD.SelectedValue.ToString().Trim() + " ", cn);
+            SqlDataAdapter ap = new SqlDataAdapter(cm);
+            DataSet dss = new DataSet();
+
+            ap.Fill(dss, "Tiet");
+            cbbTietKT.DataSource = dss.Tables[0];
+            cbbTietKT.DisplayMember = "TenTiet";
+            cbbTietKT.ValueMember = "MaTiet";
+            disconnect();
+        }
+        private void cbbTietKT_MouseEnter(object sender, EventArgs e)
+        {
+            Timekt();
+        }
+        public void PhongTrong()
+        {
+            string bd = cbbTietBD.SelectedValue.ToString().Trim();
+            string kt = cbbTietKT.SelectedValue.ToString().Trim();
+            string day = dateTimePicker1.Value.ToString("yyyy-MM-dd");
+
+            connect();
+            cm = new SqlCommand("select MaPhong, TenPhong From Phong where MaPhong not in ( select MaPhong From TKBieu where Ngay like '" + day + "' and MaPhong in (select Maphong From TKBieu where(" + bd + " >= TietBD and " + bd + " <= TietKT) or(" + kt + " >= TietBD and " + kt + " <= TietKT)))", cn);
+            ap = new SqlDataAdapter(cm);
+            ds = new DataSet();
+
+            ap.Fill(ds, "Phong");
+            cbbPhong.DataSource = ds.Tables[0];
+            cbbPhong.DisplayMember = "TenPhong";
+            cbbPhong.ValueMember = "MaPhong";
+            disconnect();
+        }
+        private void cbbPhong_MouseEnter(object sender, EventArgs e)
+        {
+            PhongTrong();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            connect();
+           
+                SqlCommand cmd = new SqlCommand("INS_MUON", cn);
+                cmd.CommandType = CommandType.StoredProcedure;
+            try
+            {
+                if (txtTen.Text.Length == 0)
+                {  throw new Exception("Tên không được rỗng !"); }
+                else
+                cmd.Parameters.Add(new SqlParameter("@ten", txtTen.Text));
+                if (txtMa.Text.Length == 0)
+                { throw new Exception("Mã số không được rỗng !"); }
+                else
+                    cmd.Parameters.Add(new SqlParameter("@ms", txtMa.Text));
+                if (txtLyDo.Text.Length == 0)
+                { throw new Exception("Lý do không được rỗng !"); }
+                else
+                    cmd.Parameters.Add(new SqlParameter("@not", txtLyDo.Text));
+                cmd.ExecuteNonQuery();
+                cmd = new SqlCommand("INS_TKB", cn);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.Add(new SqlParameter("@maphong", cbbPhong.SelectedValue.ToString().Trim()));
+                cmd.Parameters.Add(new SqlParameter("@bd", cbbTietBD.SelectedValue.ToString().Trim()));
+                cmd.Parameters.Add(new SqlParameter("@kt", cbbTietKT.SelectedValue.ToString().Trim()));
+                cmd.Parameters.Add(new SqlParameter("@date", dateTimePicker1.Value.ToString("yyyy-MM-dd")));
+                cmd.Parameters.Add(new SqlParameter("@lap", cbbLaplai.Text));
+                cmd.ExecuteNonQuery();
+                
+                MessageBox.Show("Đăng kí thành công !");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                
+
+               
+            }
+            finally
+            {
+                disconnect();
+            }
+            
+        } 
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+    }
+}
