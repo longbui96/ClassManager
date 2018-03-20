2fc79fd64dd09532e4ae736b121def11781f63af
trongtamth43 <dd01642924366@gmail.com>
2018-03-20 16:20:52 +0700

    Stored_Procedure/INS_TKBieu.sql

---
+++ Stored Procedure/INS_TKBieu.sql
@@ -0,0 +1,22 @@
+USE QLPH
+GO
+IF OBJECT_ID('INS_TKB', 'P') IS NOT NULL
+	DROP PROC INS_TKB
+GO
+
+CREATE PROC INS_TKB @maphong int=0, @bd int=0, @kt int=0, @date nvarchar(20) , @lap int=0
+AS
+BEGIN
+	DECLARE @i int = 0, @Ph int = 0
+	SELECT @Ph = MAX(MaMP) FROM MuonPhong 
+	while (@lap > @i)
+	BEGIN
+		INSERT INTO TKBieu (MaPhong, [MaLH], [TietBD], [TietKT], [Ngay], [MaMP], [MaTP]) VALUES (@maphong, 1, @bd, @kt, @date, @Ph, 1)
+		set @date = dateadd(day, 7, @date)
+		SET @i = @i + 1
+	END
+END
+GO
+
+
+
