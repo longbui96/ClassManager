3bb3faf70fe1524fa980524133396ab102067a6d
Long Bùi <long.kyanon@gmail.com>
2018-03-22 05:56:44 +0700

    Update Solution files

---
+++ TestMuonPhong/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TestMuonPhong")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TestMuonPhong")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("20fd047d-3a5d-4493-a375-d96bc0bfb62a")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ TestMuonPhong/UnitTestMuon.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RoomManager;
+
+namespace TestMuonPhong
+{
+    [TestClass]
+    public class UnitTestMuon
+    {
+        private RoomManager.Muon Muon;
+
+        [TestMethod]
+        public void TestMuon_PhongTrong()
+        {
+            //this.Muon = Muon.PhongTrong();
+        }
+    }
+}
