d43bc3ac49d19674d17e2b0430a5f5c5a5296b29
Long Bùi <long.kyanon@gmail.com>
2018-03-21 23:18:33 +0700

    Update TestQuanLy

---
+++ TestQuanLy/UnitTestQuanLy.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RoomManager;
+
+namespace TestQuanLy
+{
+    [TestClass]
+    public class UnitTestQuanLy
+    {
+        private RoomManager.Muon rm_muon;
+        [TestInitialize]
+        public void SetUp()
+        {
+            this.rm_muon = new RoomManager.Muon();
+        }
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            
+        }
+    }
+}
